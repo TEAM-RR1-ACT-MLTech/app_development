2e096c1865cf3ff23d13d11c239731a548336020
Rofhiwa <117092955+ntshagovherofhiwa@users.noreply.github.com>
2023-07-27 19:12:27 +0200

    Update edsa_recommender.py

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -116,7 +116,7 @@ def main():
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
-        st.image('resources\imgs\gg.jpg',use_column_width=True)
+        st.image('resources/imgs/gg.jpg',use_column_width=True)
         st.write('The objective is to construct a recommender system that employs content or collaborative filtering approaches to accurately forecast a users rating for a movie they have not watched, leveraging their past preferences. The aim is to develop a robust and effective solution that holds substantial economic potential, as it enables users to discover and engage with content aligned with their interests, ultimately driving revenue and fostering loyalty to the platform. For Both the collaborative and content-based filtering we implemented a few models to find a model that gives us the best rmse score which is a representation of your model performance. The model with the best rmse score was the singular value decomposition(SVD). The SVD performed better since it is very good at noise detection and does this by reducing the dimensions of a matrix in order to make certain subsequent matrix calculations simpler. By implementing SVD, which returned a very good RMSE score of 0.81 we can conclude that the algorithm implemented for our app is very good at movie recommendations.')
         
     if  page_selection == "About Us":
